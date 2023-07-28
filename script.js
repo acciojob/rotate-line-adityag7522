ddd06c15f4cea731faede1ad36823f4c61965419
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-28 14:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const lineElement = document.getElementById('line');
+
+function rotateLine() {
+    let rotation = 0;
+    const rotationInterval = 2; 
+    
+    function animateRotation() {
+        rotation += rotationInterval;
+        lineElement.style.transform = `rotate(${rotation}deg)`;
+        requestAnimationFrame(animateRotation);
+    }
+
+    animateRotation();
+}
+
+window.onload = rotateLine();
